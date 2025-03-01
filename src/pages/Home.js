2fe9021b97a3c9d3c9daaec45e8ce3a42e8e6e9d
sarorosy <sn11std@gmail.com>
2025-03-01 22:23:48 +0530

    new

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -147,7 +147,7 @@ const Home = () => {
                   {/* Right side favorite button */}
                   <button onClick={() => handleFavorite(strain.id)} className="text-teal-600 hover:text-teal-700 border border-teal-600 rounded-full p-1 flex-shrink-0 hover:scale-105 transition-all duration-300">
                     {userFavourites.includes(strain.id) ? (
-                      <Star size={18}>
+                      <Star size={18} />
                     ) : (<svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                     </svg>)}
